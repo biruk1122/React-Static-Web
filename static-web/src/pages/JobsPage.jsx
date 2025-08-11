56072a17c4c0623da480dfe822ad79b07af29d07
biruk1122 <birukfikadu29@gmail.com>
2025-08-11 20:49:30 +0300

    a little some update

--- static-web/src/pages/JobsPage.jsx
+++ static-web/src/pages/JobsPage.jsx
@@ -1,3 +1,11 @@
-function JobsPage() {}
+import JobListings from "../components/JobListings"
+
+function JobsPage() {
+  return (
+    <section className="bg-blue-50 px-4 py-6">
+      <JobListings />
+    </section>
+  )
+}
 
 export default JobsPage
